5768f51ed31316cbdde28653e84e3c113fe4b196
nepta <nepta23@gmail.com>
2015-02-02 00:50:16 +0100

    added some color

--- src/Client/Client.cpp
+++ src/Client/Client.cpp
@@ -15,8 +15,16 @@ int main(int argc, char* argv[]){
 	raw();
 	keypad(stdscr, TRUE);
 	noecho();
+	start_color();
+	init_pair(1, COLOR_RED, COLOR_BLACK);
+	init_pair(2, COLOR_GREEN, COLOR_BLACK);
+	
+	attron(COLOR_PAIR(1));
 	box(stdscr,0,0);
+	attroff(COLOR_PAIR(1));
+	attron(COLOR_PAIR(2));
 	mvprintw(LINES/2, COLS/2, response.c_str());
+	attroff(COLOR_PAIR(2));
 	refresh();
 	getch();
 	endwin();
