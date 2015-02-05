ecda59648aa198a2a00174fe09560a26efda548d
nepta <nepta23@gmail.com>
2015-02-05 19:42:58 +0100

    show list of member

--- src/Client/Client.cpp
+++ src/Client/Client.cpp
@@ -8,27 +8,28 @@
 
 
 int main(int argc, char* argv[]){
-//	Socket socket("client");
+	Socket socket("client");
 //	socket << std::string("addUser(Flandre,18)");
-//	socket << std::string("displayMembers()");
+	socket << std::string("displayMembers()");
 //	socket << std::string("getUserBalance(Flandre)");
-//	std::string response;
-//	socket >> response;
-	std::string response = "Flandre : 18";
-	Gui gui;
-	
-	gui.mvprintw(LINES/2, COLS/3, response.c_str());
+	std::string response;
+	socket >> response;
+	std::cout << response;
+//	std::string response = "Flandre : 18";
+//	Gui gui;
+//	
+//	gui.mvprintw(LINES/2, COLS/3, response.c_str());
 
 
-	GuiMenu menu;
-	menu.addItem("Cirno","ice fairy");
-	menu.addItem("Suwako","frog");
-	menu.createMenu();
+//	GuiMenu menu;
+//	menu.addItem("Cirno","ice fairy");
+//	menu.addItem("Suwako","frog");
+//	menu.createMenu();
 
-	int ch;
-	gui >> ch;
-	menu.down();
-	gui >> ch;
-//	socket << std::string("quit");
+//	int ch;
+//	gui >> ch;
+//	menu.down();
+//	gui >> ch;
+	socket << std::string("quit");
 	return EXIT_SUCCESS;
 }
