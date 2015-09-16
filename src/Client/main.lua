6d993fa4f418b3d1f8e7f92419b6e22d11bb2319
nepta <nepta23@gmail.com>
2015-09-17 01:44:41 +0200

    added selected button

--- src/Client/main.lua
+++ src/Client/main.lua
@@ -6,10 +6,14 @@ Grid = require "Gui.Grid"
 function love.load()
 	Model:init(ZMQDriver)
 	local position = Grid:init({offset={x=50, y=200}})
+	selectedButton = nil
 	for memberString in Model:memberList() do
 		local pseudo, money = Model:parse(memberString)
 		local x,y = position()
-		ButtonManager:newButton{x=x, y=y, account= pseudo, credit= money}
+		local button = ButtonManager:newButton{x=x, y=y, account= pseudo, credit= money}
+		button.click = function(self)
+			selectedButton = self
+		end
 	end
 	ButtonManager:pagination()
 	love.graphics.setBackgroundColor(13, 64, 126)
